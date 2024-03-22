62fa28445e966a1e4b5040e83ce3d6cae64e2d11
buseseker <sekerbuse_96@hotmail.com>
2024-03-22 16:03:32 +0300

    Some imported file has been removed

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from '../src/App'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
